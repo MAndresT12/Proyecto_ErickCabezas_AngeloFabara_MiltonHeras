6bd85036adba4a7bb48c0df26a53442e3bf0cee8
Milton <Milton@DESKTOP-QS9FVO3>
2021-08-31 21:38:19 -0500

    Milton Heras: prueba

--- src/testproyecto/GUIGrupo.java
+++ src/testproyecto/GUIGrupo.java
@@ -86,7 +86,7 @@ public class GUIGrupo extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        JOptionPane.showMessageDialog(rootPane, "Lider: Milton Heras, username: MAndresT12");  
+        JOptionPane.showMessageDialog(rootPane, "Lider: Milton Heras, username: MAndresT12, ols");  
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
