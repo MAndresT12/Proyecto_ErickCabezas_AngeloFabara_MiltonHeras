6baff335b660737b0fc0673b9b26f1f249ab0586
erick cabezas <erick cabezas@MARDIXPRO>
2021-09-03 16:30:19 -0500

    Calcular el precio de alquiler en computadoras

---
+++ src/poo/clases/proyecto/Computadora.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.clases.proyecto;
+
+/**
+ *
+ * @author Milton
+ */
+public class Computadora extends Maquina{
+    private String tipoDeComputadora;
+
+    public void setTipoDeComputadora(String tipoDeComputadora) {
+        this.tipoDeComputadora = tipoDeComputadora;
+        if("Computadora Gamer".equals(this.tipoDeComputadora)){
+            this.setValorPorHora(1.50);
+        }
+        if("Computadora Oficina".equals(this.tipoDeComputadora)){
+            this.setValorPorHora(0.75);
+        }
+    }
+    
+    public String getTipoDeComputadora() {
+        return tipoDeComputadora;
+    }
+
+    public int getHorasDealquiler() {
+        return horasDealquiler;
+    }
+
+    public void setHorasDealquiler(int horasDealquiler) {
+        this.horasDealquiler = horasDealquiler;
+    }
+
+    public double getValorPorHora() {
+        return valorPorHora;
+    }
+
+    public void setValorPorHora(double valorPorHora) {
+        this.valorPorHora = valorPorHora;
+    }
+     
+    @Override
+    public Double calcularPago() {
+        this.cobrado=valorPorHora*horasDealquiler;
+        return this.cobrado;
+    }
+    
+}
