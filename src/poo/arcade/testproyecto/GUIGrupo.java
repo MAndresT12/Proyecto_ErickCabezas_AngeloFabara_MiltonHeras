e3eb2fb610ab576307b0849338af8c4c8e55d73e
erick cabezas <erick cabezas@MARDIXPRO>
2021-09-03 14:43:04 -0500

    Renombrer el paquete

--- src/poo/arcade/testproyecto/GUIGrupo.java
+++ src/poo/arcade/testproyecto/GUIGrupo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testproyecto;
+package poo.arcade.testproyecto;
 
 import javax.swing.JOptionPane;
 
