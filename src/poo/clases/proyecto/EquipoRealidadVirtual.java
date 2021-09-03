3fd861fcbe700b8fe4f1c62a344a96ed2ac3994e
Milton <Milton@DESKTOP-QS9FVO3>
2021-09-03 17:30:52 -0500

    Milton Heras: Asignacion de datos de equipos de realidad virtual, con sus valores correspondientes dependiendo de tipo y simulacion.

--- src/poo/clases/proyecto/EquipoRealidadVirtual.java
+++ src/poo/clases/proyecto/EquipoRealidadVirtual.java
@@ -12,9 +12,58 @@ package poo.clases.proyecto;
 public class EquipoRealidadVirtual extends Maquina{
     private String tipoDeEquipoRV;
     private double valorJuegoRV;
+    private String tipoDeSimulacion;
+    
+    public void setTipoDeConsola(String tipoDeEquipoRV) {
+        this.tipoDeEquipoRV = tipoDeEquipoRV;
+        if("Nintendo Labo VR".equals(this.tipoDeEquipoRV)){
+            this.valorPorHora =4;
+        }
+        if("Valve Index".equals(this.tipoDeEquipoRV)){
+            this.valorPorHora =4.80;
+        }
+        if("Oculus Rift ".equals(this.tipoDeEquipoRV)){
+            this.valorPorHora =5.60;
+        }     
+        if("HTC Vive".equals(this.tipoDeEquipoRV)){
+            this.valorPorHora =5.75;
+        }   
+        if("Oculus Go".equals(this.tipoDeEquipoRV)){
+            this.valorPorHora =4.25;
+        }             
+    }   
+
+    public void setTipoDeSimulacion(String tipoDeSimulacion) {
+        this.tipoDeSimulacion = tipoDeSimulacion;
+        if("Conduccion".equals(this.tipoDeSimulacion)){
+            this.valorJuegoRV = 3.50;
+        }
+        if("Shooter".equals(this.tipoDeSimulacion)){
+            this.valorJuegoRV =1.50;
+        }
+        if("Visualizacion de entornos".equals(this.tipoDeSimulacion)){
+            this.valorJuegoRV =0.50;
+        }     
+        if("Patinaje".equals(this.tipoDeSimulacion)){
+            this.valorJuegoRV =2.25;
+        }          
+    }
+    public int getHorasDealquiler() {
+        return horasDealquiler;
+    }
+
+    public double getValorPorHora() {
+        return valorPorHora;
+    }
+    
+    public void setHorasDealquiler(int horasDealquiler) {
+        this.horasDealquiler = horasDealquiler;
+    }
+    
     
     @Override
     public Double calcularPago() {
-        return this.valorJuegoRV+(this.horasDealquiler*this.valorPorHora);
+        this.cobrado = this.valorJuegoRV+(this.horasDealquiler*this.valorPorHora);
+        return this.cobrado;
     }
 }
